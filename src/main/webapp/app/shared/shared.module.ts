b63ca25115264da32238c9fa27346fd33dc177ef
Rodrigo Costa <rodrigomcosta20@gmail.com>
2024-02-16 03:40:17 -0300

    Adicionar dependencia angular material

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -4,12 +4,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
+import { JhMaterialModule } from './jh-material.module';
 
 /**
  * Application wide Module
  */
 @NgModule({
   imports: [AlertComponent, AlertErrorComponent],
-  exports: [CommonModule, NgbModule, FontAwesomeModule, AlertComponent, AlertErrorComponent],
+  exports: [JhMaterialModule, CommonModule, NgbModule, FontAwesomeModule, AlertComponent, AlertErrorComponent],
 })
 export default class SharedModule {}
